1c456f19c5ab3b5214471fac76039a26a50b1478
Lauren Tsao <et2676@nyu.edu>
2024-10-19 00:50:29 -0400

    Mushrooms done

--- sketch.js
+++ sketch.js
@@ -1,6 +1,51 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+let data = [];
+
+function preload() {
+  data = loadTable("mushroom.csv", "csv", "header");
 }
 
-function draw() {
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(233, 238, 217);
+  
+  angleMode(DEGREES)
+
+  
+
+  for (let idx = 0; idx < data.getRowCount(); idx++) {
+    let x = map(idx, 0, data.getRowCount() - 1, 0, width);
+    let capColor = data.getString(idx, "cap-color")
+    let stemHeight = data.getNum(idx, "stem-height");
+    let stemWidth = data.getNum(idx, "stem-width")
+    let capDiameter = data.getNum(idx, "cap-diameter");
+    let h = map(stemHeight, 0, 20, 0, 400);
+    let thicc = map(stemWidth, 0, 20, 0, 3)
+    let d = map(capDiameter, 0, 200, 0, height);
+    stroke(84, 71, 63);
+    strokeWeight(thicc)
+    line(x, height - height/9, x, h);
+    noStroke()
+    arc(x, h, d, d - 10, -180, 0, CHORD);
+
+    if (capColor.includes("o")) {
+      fill(154, 126, 111);
+    } else if (capColor.includes("e")) {
+      fill(214, 192, 179);
+    }
+  }
+
+  for (let xGrass = 0; xGrass < width; xGrass += 3) {
+    let lightG = color(203, 210, 164)
+    let darkG = color(121, 134, 69)
+    let strokeColor = random([lightG, darkG])
+    let grassHeight = random(410, 400)
+    stroke(strokeColor)
+    strokeWeight(3)
+    line(xGrass, height - height/9, xGrass, grassHeight)
+  }
+  fill(84, 71, 63);
+  noStroke()
+  rect(0, height - height/9, width, height - height / 9)
 }
+
+function draw() {}
